0441300c1febf757b77ab41ae8523b4798db17b0
Lars Olesen <lars@intraface.dk>
2013-08-23 08:42:38 +0200

    Updated to Omega 4

--- template.php
+++ template.php
@@ -2,11 +2,6 @@
 
 /**
  * @file
- * This file is empty by default because the base theme chain (Alpha & Omega) provides
- * all the basic functionality. However, in case you wish to customize the output that Drupal
- * generates through Alpha & Omega this file is a good place to do so.
- * 
- * Alpha comes with a neat solution for keeping this file as clean as possible while the code
- * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
- * for more information on this topic.
+ * Template overrides as well as (pre-)process and alter hooks for the
+ * Score theme.
  */
